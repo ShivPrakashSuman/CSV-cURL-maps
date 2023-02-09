5124a791a5f026085fd168d3f13e219e35a9b896
shiv-prakash742 <121678355+shiv-prakash742@users.noreply.github.com>
2023-02-09 15:30:54 +0530

    table color updat

--- pdfData.php
+++ pdfData.php
@@ -14,14 +14,17 @@
 
         // Add a new page 
         $pdf->AddPage();
-        $pdf->SetFont('Arial','B', 25); 
+
+        $pdf->SetDrawColor(1,80,180); // Border Color set ----
+
+        $pdf->SetFont('Times','B', 25); 
         $pdf->Cell(40,20,'Company Name');
         $pdf->Cell(260,20,'INVOICE','0','1','C');
 
         $pdf->SetFont('Arial','I', 13); 
         $pdf->Cell(40,0,'Company Siogan....','0','1');
         $pdf->ln(14); // line chenj 
-
+        
         $pdf->SetFont('Arial','B', 15); 
         $pdf->Cell(40,6,'STREET ADDRESS','0','0');
         $pdf->Cell(103,6,'DATA','0','0','R');
@@ -45,7 +48,8 @@
         $pdf->ln(10); // line chenj 
         $pdf->SetFont('Arial','B', 15); 
         $pdf->Cell(40,10,'Bill To :','0','1');
-
+       
+        $pdf->Image('home.JPG',150,80,50);
         $pdf->SetFont('Arial', 'I', 13);
         $pdf->Cell(30, 6,'Name     = '.$data['name'], 0,'C');
         $pdf->Cell(30, 6,'Email      = '.$data['email'], 0, 'C');
@@ -55,24 +59,35 @@
       // table  ---------------
         $pdf->ln(15); // line chenj 
         $pdf->SetFont('Arial','B', 15);
-       // $pdf->setfillcolor('red'); 
-        $pdf->cell(150, 9, 'DESCRIPTION',1 ,0 ,'C');
-        $pdf->cell(40, 9, 'AMOUNT',1 ,1 ,'C');
-        $pdf->cell(150, 80, '',1 ,0 ,'C');
-        $pdf->cell(40, 80, '',1 ,1 ,'C');
+        $pdf->SetTextColor(10,80,120);   // Text Color ---
+        $pdf->cell(150,9, 'DESCRIPTION',1 ,0 ,'C');
+        $pdf->cell(40,9, 'AMOUNT',1 ,1 ,'C');
+        $pdf->cell(150,10, '','LR' ,0 ,'C');
+        $pdf->cell(40,10, '500','LR','1','C');
+        $pdf->cell(150,10, '','LR' ,0 ,'C'); 
+        $pdf->Cell(40,10,'400','LR','1','C'); 
+        $pdf->cell(150,50, '','LRB' ,0 ,'C'); 
+        $pdf->Cell(40,50,'','LR','1','C');
+        $pdf->SetTextColor(220,50,50);   // Text Color ---
         $pdf->cell(150, 9, 'TOTAL',0 ,0 ,'R');
-        $pdf->cell(40, 9, '$ - ',1 ,1 ,'L');
+        $pdf->cell(1, 9, '$ - ','TLB' ,0 ,'L');
+        $pdf->cell(39, 9, '900','RBT' ,1 ,'C');
 
+        $pdf->SetTextColor(0,0,0);
         $pdf->ln(12); // line chenj 
         $pdf->SetFont('Arial','I', 13);
         $pdf->cell(55, 9, 'Make All check payable to',0 ,0 ,'L');
         $pdf->SetFont('Arial','B', 14);
         $pdf->cell(0, 9, 'Your Company Name',0 ,1 ,'L');
 
-        $pdf->line(10, 250, 200, 250 );
+        $pdf->line(10, 240, 200, 240 );
         $pdf->ln(15); // line chenj 
         $pdf->SetFont('Arial','B', 15);
-        $pdf->cell(200, 9, 'THANK YOU FOR YOUR BUSINESS!',0 ,0 ,'C');
+        $pdf->cell(0, 9, 'THANK YOU FOR YOUR BUSINESS!',0 ,0 ,'C');
+
+//$pdf->SetFillColor(230,230,0);  // back color
+//$pdf->AddLink();
+//$pdf->SetLink( 'http://www.fpdf.org/')
 
         $pdf->Output();
     }
